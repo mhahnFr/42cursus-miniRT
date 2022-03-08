c6634d630f018390e0e91c4e1034b804da1f6133
mhahn <mhahn@student.42heilbronn.de>
2022-03-08 19:45:15 +0100

    Fixed delegate creation

--- src/delegate.c
+++ src/delegate.c
@@ -1,11 +1,17 @@
+#include <stdlib.h>
+
 #include "minirt.h"
 
-t_delegate	*init_mainstruct(char *file, int *ret)
+t_delegate	*init_mainstruct(char *file, int *err)
 {
 	t_delegate	*ret;
 
+	(void) file;
 	ret = malloc(sizeof(struct s_delegate));
 	if (ret == NULL)
+	{
+		*err = 3;
 		return (NULL);
+	}
 	return (ret);
 }
