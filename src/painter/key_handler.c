3541ddfd6a42efd921be5ef970307207f5dfaa91
mhahn <mhahn@student.42heilbronn.de>
2022-04-13 14:06:30 +0200

    Freeing some stuff

--- src/painter/key_handler.c
+++ src/painter/key_handler.c
@@ -6,7 +6,7 @@
 /*   By: jkasper <jkasper@student.42Heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 18:15:32 by jkasper           #+#    #+#             */
-/*   Updated: 2022/04/08 18:12:37 by mhahn            ###   ########.fr       */
+/*   Updated: 2022/04/13 14:05:09 by mhahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,22 @@
 
 void	key_escape(t_mixer *p_null)
 {
+	t_obj_l	*tmp;
+	t_obj_l	*tmp2;
+	int		i;
+
 	mlx_destroy_window(p_null->p_mlx_init, p_null->p_mlx_window);
 	renderer_image_delete(p_null->image);
+	tmp = p_null->obj_list;
+	while (tmp != NULL)
+	{
+		tmp2 = tmp->next;
+		ft_gc_free(tmp2);
+		tmp = tmp2;
+	}
+	ft_gc_free(p_null->col_sum.sum);
+	ft_gc_free(p_null->col_sum.fac);
+	ft_gc_free(p_null);
 	ft_gc_exit(0);
 }
 
