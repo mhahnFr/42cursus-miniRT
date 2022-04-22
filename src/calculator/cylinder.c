9eeef9d768567ac4c7f5c307d187ad907c222ea3
jkasper <jkasper@student.42heilbronn.de>
2022-04-22 19:35:59 +0200

    added feature: cylinder has now caps! todo: fix shadows. prob. cylinder still wrong

--- src/calculator/cylinder.c
+++ src/calculator/cylinder.c
@@ -83,7 +83,69 @@ t_vector	cylinder_intersect_normal(t_vector *origin, t_vector *inter, t_vector *
 	return (ret);
 }
 
-bool	hit_cylinder(
+bool	hit_cylinder_top(
+		t_vector *origin, t_obj_l *obj, t_vector *ray, t_vector *sect)
+{
+	t_vector	obj_normal;
+	t_vector	obj_position;
+	t_vector	inter;
+	float 		d;
+
+	obj_normal = obj->normal;
+	vector_multiply_digit(&obj_position, &obj_normal, obj->height);
+	vector_addition(&obj_position, &obj_position, &obj->position);
+	vector_substract(&inter, &obj_position, origin);
+	d = vector_scalar_product(&inter, &obj_normal) / vector_scalar_product(ray, &obj_normal);
+	obj->disthit = d;
+	//if (vector_scalar_product(ray, &obj_normal) > 0)
+	//vector_multiply_digit(&obj->col_normal, &obj_normal, -1);
+	//else
+	obj->col_normal = obj_normal;
+	vector_multiply_digit(&inter, ray, d);
+	vector_addition(sect, origin, &inter);
+	vector_substract(&inter, sect, &obj_position);
+	return (vector_length(&inter) < obj->width);
+}
+
+	//obj->position;
+bool	hit_cylinder_bottom(
+		t_vector *origin, t_obj_l *obj, t_vector *ray, t_vector *sect)
+{
+	t_vector	obj_normal;
+	t_vector	inter;
+	float 		d;
+
+	vector_multiply_digit(&obj_normal, &obj->normal, -1);
+	vector_substract(&inter, &obj->position, origin);
+	d = vector_scalar_product(&inter, &obj_normal) / vector_scalar_product(ray, &obj_normal);
+	obj->disthit = d;
+	//if (vector_scalar_product(ray, &obj_normal) > 0)
+	//vector_multiply_digit(&obj->col_normal, &obj_normal, -1);
+	//else
+		obj->col_normal = obj_normal;
+	vector_multiply_digit(&inter, ray, d);
+	vector_addition(sect, origin, &inter);
+	vector_substract(&inter, sect, &obj->position);
+	return (vector_length(&inter) < obj->width);
+}
+
+bool	hit_cylinder_caps(
+			t_vector *origin, t_obj_l *obj, t_vector *ray, t_vector *sect)
+{
+	t_vector	inter;
+	bool		top;
+	bool		bot;
+
+	top = hit_cylinder_top(origin, obj, ray, sect);
+	if (top)
+		inter = *sect;
+	bot = hit_cylinder_bottom(origin, obj, ray, sect);
+	if (top && bot && vector_length(&inter) < vector_length(sect))
+		*sect = inter;
+	return (top || bot);
+}
+
+bool	hit_cylinder_mantel(
 			t_vector *origin, t_obj_l *obj, t_vector *ray, t_vector *inter)
 {
 	float	a;
@@ -91,7 +153,7 @@ bool	hit_cylinder(
 	float	c;
 	float	d;
 	float	root;
-	
+
 	a = cylinder_part_a(ray, &obj->normal);
 	b = cylinder_part_b(ray, &obj->normal, origin, &obj->position);
 	c = cylinder_part_c(*origin, obj->position, obj->normal, obj->width);
@@ -124,4 +186,21 @@ bool	hit_cylinder(
 	}
 	return (false);
 }
+
 //-b +- sqrtf(bˆ2 - 4ac) / 2a;
+
+bool	hit_cylinder(
+			t_vector *origin, t_obj_l *obj, t_vector *ray, t_vector *sect)
+{
+	t_vector	inter;
+	bool		mat;
+	bool		cap;
+
+	mat = hit_cylinder_mantel(origin, obj, ray, sect);
+	if (mat)
+		inter = *sect;
+	cap = hit_cylinder_caps(origin, obj, ray, sect);
+	if (mat && cap && vector_length(&inter) < vector_length(sect))
+		*sect = inter;
+	return (mat || cap);
+}
