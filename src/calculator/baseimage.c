a05f876c97b1a9345a0efdf941941904f6babfe4
jkasper <jkasper@student.42heilbronn.de>
2022-04-22 15:56:15 +0200

    adjusted intersect_next for cylinder to be the same as other objects

--- src/calculator/baseimage.c
+++ src/calculator/baseimage.c
@@ -36,9 +36,10 @@ bool	intersec_next(
 	else if (objs->obj_type == PLANE && fast_intersec_plane(ray, &objs->normal))
 		ret = intersec_plane(ray, origin, objs, inter);
 	else if (objs->obj_type == CYLINDER)
-		return (hit_cylinder(origin, objs, ray, inter));
+		ret = hit_cylinder(origin, objs, ray, inter);
 	//else if (objs->obj_type == LIGHT)
 		//	ret = specular_highlight(origin, objs, ray, inter);
+
 	return (ret);
 }
 
